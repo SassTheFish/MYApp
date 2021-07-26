15070d574033b3b4522c73d2a63a463352f1064d
Aleks <aleksander.roosimaa@gmail.com>
2021-07-26 22:19:56 +0300

    cool background, logo, more routing cleanup, css cleanup, error page, index file cleanup, harjutus uuenda page

--- Routes/harjutusRouter.js
+++ Routes/harjutusRouter.js
@@ -1,25 +1,80 @@
 const express = require('express')
 const {Harjutus} = require('../models/harjutus');
-
+const {TreeningKava} = require('../models/harjutus');
 const router = express.Router();
+const { c_lihasgruppid } = require('../variables');
 
-const lihasgruppid = ['jalad', 'kere', 'käed'];
 
-
-router.get("/harjutused", async (req, res)=>{
-    
+router.get("/", async (req, res)=>{
+    const length = c_lihasgruppid.length;
+    const half1_lg = c_lihasgruppid.slice(0,length/2);
+    const half2_lg = c_lihasgruppid.slice(length/2, length);
     const reqbody = req.body;
     const path = req.path;
     const harjutused = await Harjutus.find();
-    const object = 
+    const object =
     {
         reqbody,
         path,
         harjutused,
-        lihasgruppid
+        c_lihasgruppid,
+        half1_lg,
+        half2_lg
     }
-    res.render("harjutused", {...object})
+    res.render("Harjutused/harjutused", {...object})
 })
+
+router.get("/deleteOne/:id", async (req,res)=>{
+    const {id} = req.params;
+    const kavad = await TreeningKava.find();
+    let usedharj = false;
+    for(kava of kavad){
+        for(harjutus of kava.harjutused)
+        {
+            if(harjutus.harj == id){usedharj = true}
+        }
+    }
+    if(!usedharj)
+    {
+        await Harjutus.findByIdAndDelete(id).then(()=>{console.log("done")}).catch(err=>{console.log("err", err)});
+    } else {
+        console.log("Kava on kasutusel");
+    }
+    res.redirect("/harjutused")
+})
+
+router.get("/update/:id", async(req,res)=>{
+    const {id} = req.params;
+    const harjutus = await Harjutus.findById(id);
+    const object =
+    {
+         harjutus,
+         c_lihasgruppid
+    }
+    res.render("Harjutused/uuendaHarjutust", {...object})
+})
+
+//delete all entries !!!!!!!_______KÕIK MURDUB________!!!!!
+router.get("/deleteAll", async(req,res)=>{
+    console.log("deleting...")
+    await Harjutus.deleteMany({}).then(res=>{console.log("Done")}).catch(err=>{console.log(err)});
+    res.redirect("../harjutused")
+})
+
+router.put("/update/:id", async (req,res)=>{
+    const {id} = req.params;
+    const updated = await Harjutus.findByIdAndUpdate(id, req.body, {runValidators:true, new:true});
+    res.redirect("/harjutused")
+})
+
+router.post("/", async (req,res) => {
+    const harjutus = req.body;
+    console.log(harjutus);
+    const harjutus1 = await new Harjutus(harjutus);
+    harjutus1.save().then(res =>{console.log(res)}).catch(err => {console.log(err)});
+    res.redirect("/harjutused");
+})
+
 
 
 module.exports = router;
--- index.js
+++ index.js
@@ -1,30 +1,44 @@
-const express = require("express");
+//---------EXTERNAL----------
 const { url } = require("inspector");
 const { dirname } = require("path");
 const path  = require("path");
-const app = express();
+const { object } = require("joi");
 
+const express = require("express");
+const app = express();
 const methodOverride = require("method-override")
+
 const mongoose = require("mongoose");
 const morgan = require("morgan")
 const ejsMate = require("ejs-mate");
+
+
+
+//------ MY STUFF -----------
+
+//ERROR MANAGEMENT
 const catchAsync = require("./Utils/CatchAsync");
 const ExpressError = require('./Utils/ExpressError');
 
-
+//VARIABLES
 const {c_lihasgruppid} = require('./variables');
 
+//MODELS
 
 
+
+//ROUTER
+const homeRouter = require('./Routes/homeRouter');
+const ukekavaRouter = require('./Routes/ukekavadRouter');
+const ujumisekavaRouter = require('./Routes/ujumisekavaRouter');
+const harjutusteRouter = require('./Routes/harjutusRouter');
 
 
 
 
 morgan('tiny');
 
-const {Harjutus} = require('./models/harjutus');
-const {TreeningKava} = require('./models/harjutus');
-// const {UjumisTreening} = require('./models/ujumistreening');
+
 
 mongoose.connect('mongodb://localhost:27017/MYAPP', {useNewUrlParser:true, useUnifiedTopology:true})
     .then(()=>{
@@ -36,11 +50,10 @@ mongoose.connect('mongodb://localhost:27017/MYAPP', {useNewUrlParser:true, useUn
     })
 
 
-const homeRouter = require('./Routes/homeRouter');
-const ukekavaRouter = require('./Routes/ukekavadRouter');
-const ujumisekavaRouter = require('./Routes/ujumisekavaRouter');
 
 
+
+//--------------MIDDLEWARE-----------------
 
 app.engine('ejs', ejsMate);
 app.set("view engine", "ejs");
@@ -52,18 +65,12 @@ app.use(methodOverride('_method'))
 app.use(morgan('dev'));
 
 
+//--------------ROUTERS--------------
+
 app.use('/', homeRouter);
 app.use('/ukekavad', ukekavaRouter);
 app.use('/ujumisekavad', ujumisekavaRouter);
-
-
-const verify = ((req,res,next)=>{
-    const {password} = req.query;
-    if(password === "chicken"){next();}
-    res.send("NOPE")
-})
-
-
+app.use('/harjutused', harjutusteRouter);
 
 
 
@@ -87,68 +94,6 @@ app.get("/example", (req,res)=>{
     res.render("example.ejs")
 })
 
-app.get("/harjutused", async (req, res)=>{
-    const length = c_lihasgruppid.length;
-    const half1_lg = c_lihasgruppid.slice(0,length/2);
-    const half2_lg = c_lihasgruppid.slice(length/2, length);
-    const reqbody = req.body;
-    const path = req.path;
-    const harjutused = await Harjutus.find();
-    const object =
-    {
-        reqbody,
-        path,
-        harjutused,
-        c_lihasgruppid,
-        half1_lg,
-        half2_lg
-    }
-    res.render("Harjutused/harjutused", {...object})
-})
-
-
-
-//delete all entries !!!!!!!_______KÕIK MURDUB________!!!!!
-app.get("/harjutused/deleteAll", async(req,res)=>{
-    console.log("deleting...")
-    await Harjutus.deleteMany({}).then(res=>{console.log("Done")}).catch(err=>{console.log(err)});
-    res.redirect("../harjutused")
-})
- 
-app.get("/harjutused/deleteOne/:id", async (req,res)=>{
-    const {id} = req.params;
-    const kavad = await TreeningKava.find();
-    let usedharj = false;
-    for(kava of kavad){
-        for(harjutus of kava.harjutused)
-        {
-            if(harjutus.harj == id){usedharj = true}
-        }
-    }
-    if(!usedharj)
-    {
-        await Harjutus.findByIdAndDelete(id).then(()=>{console.log("done")}).catch(err=>{console.log("err", err)});
-    } else {
-        console.log("Kava on kasutusel");
-    }
-    res.redirect("/harjutused")
-})
-
-
-
-//Choose one to update
-
-
-app.get("/harjutused/updateH/:id", async(req,res)=>{
-    const {id} = req.params;
-    const harjutus = await Harjutus.findById(id);
-    const object =
-    {
-         harjutus,
-         c_lihasgruppid
-    }
-    res.render("Harjutused/uuendaHarjutust", {...object})
-})
 
 
 app.get("/technique-guides", (req, res)=>{
@@ -162,7 +107,7 @@ app.get("/technique-guides", (req, res)=>{
     }
     res.render("technique-guides.ejs", {...object})
 })
-app.get("/swimming-plans", verify, (req, res)=>{
+app.get("/swimming-plans", (req, res)=>{
     
     const reqbody = req.body;
     const path = req.path;
@@ -174,17 +119,11 @@ app.get("/swimming-plans", verify, (req, res)=>{
     res.render("swimming-plans.ejs", {...object})
 })
 
+
+
 //------------PUT------------------
 
-
-
-//Update that one and return
-app.put("/harjutused/updateH/:id", async (req,res)=>{
-    const {id} = req.params;
-    const updated = await Harjutus.findByIdAndUpdate(id, req.body, {runValidators:true, new:true});
-    res.redirect("Harjutused/harjutused")
-})
-
+//?
 app.put("/harjutused/updateK/:id", async (req,res)=>{
     const {id} = req.params;
     const updated = await TreeningKava.findByIdAndUpdate(id, req.body, {runValidators:true, new:true});
@@ -195,27 +134,25 @@ app.put("/harjutused/updateK/:id", async (req,res)=>{
 
 
 
-
-
-
-
 //---------------POST-------------------
 
-//make one and return to harjutused
-app.post("/harjutused", async (req,res) => {
-    const harjutus = req.body;
-    console.log(harjutus);
-    const harjutus1 = await new Harjutus(harjutus);
-    harjutus1.save().then(res =>{console.log(res)}).catch(err => {console.log(err)});
-    res.redirect("harjutused");
-})
 
 
 
 
+
+
+
+
+
+
+//--------------------------------------
+//LAST CALL
 app.use((err,req,res,next)=>{
-    console.log(err);
-    res.send('something went wrong')
+    const object = {
+        err
+    }
+    res.render("ERROR", {...object});
 })
 
 
